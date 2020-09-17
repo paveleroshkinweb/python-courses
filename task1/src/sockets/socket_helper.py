c31a103b929b04910411276a0b4c5e86ab950da4
paveleroshkinweb <eroshkin321@gmail.com>
2020-09-18 00:13:15 +0300

    added lock and broadcast for socket base class

--- task1/src/sockets/socket_helper.py
+++ task1/src/sockets/socket_helper.py
@@ -1,61 +1,57 @@
 import socket
 import logging
 import threading
-import struct
-import json
+from contextlib import suppress
 from exceptions.socket_closed import SocketIOError
 from models.message import Message
+from models.entity import Entity
+from utils.lock import lock
 
 
+@lock(['close', 'send_message', 'receive_message', 'broadcast_message', 'send'], threading.RLock)
 class SocketHelper:
-
-    header_struct = struct.Struct('!I')
 
     def __init__(self, sock):
         self.socket = sock
         self.closed = False
-        self.lock = threading.RLock()
 
     def close(self):
-        with self.lock:
-            if not self.closed:
-                try:
-                    self.socket.close()
-                    self.closed = True
-                except socket.error as e:
-                    logging.error(f"Couldn't close socket {self.socket.getsockname}\n{e}")
+        try:
+            self.socket.close()
+            self.closed = True
+        except socket.error as e:
+            logging.error(f"Couldn't close socket {self.socket.getsockname}\n{e}")
 
     def send_message(self, message):
-        with self.lock:
-            if self.closed:
-                raise SocketIOError(f"Can't send message \n{message}\n due to closed socket")
-            json_message = message.to_json()
-            packet = json_message.encode()
-            packet_length = SocketHelper.header_struct.pack(len(packet))
-            try:
-                self.socket.send(packet_length)
-                self.socket.sendall(packet)
-            except socket.error as e:
-                raise SocketIOError(f"Can't send \n{message}\n, {e}")
+        packet_length, packet = message.to_bytes()
+        self.send(packet_length, packet)
 
     def receive_message(self):
-        with self.lock:
-            if self.closed:
-                raise SocketIOError(f"Can't receive message due to closed socket")
-            message_length_bytes = self._recv_all(SocketHelper.header_struct.size)
-            message_length = SocketHelper.header_struct.unpack(message_length_bytes)[0]
-            message_bytes = self._recv_all(message_length)
-            message_json = message_bytes.decode()
-            message = Message(**json.loads(message_json))
-            return message
+        message_length_bytes = self._recv_all(Entity.header_struct.size)
+        message_length = Entity.header_struct.unpack(message_length_bytes)[0]
+        message_bytes = self._recv_all(message_length)
+        message = Message.from_bytes(message_bytes)
+        return message
+
+    def broadcast_message(self, message, sockets):
+        packet_length, packet = message.to_bytes()
+        for sock in sockets:
+            if self.socket != sock:
+                with suppress(SocketIOError):
+                    sock.send(packet_length, packet)
+                    yield message, sock  # ??? if I need it
+
+    def send(self, length_bytes, message_bytes):
+        try:
+            self.socket.send(length_bytes)
+            self.socket.sendall(message_bytes)
+        except socket.error as e:
+            raise SocketIOError(f"Can't send \n{message_bytes.decode()}\n, {e}")
 
     def _recv_all(self, length):
         data = b''
         while len(data) < length:
-            try:
-                chunk = self.socket.recv(length)
-            except socket.error as e:
-                raise SocketIOError(f"Can't get message because of {e}")
+            chunk = self.socket.recv(length)
             if not chunk:
                 raise SocketIOError(
                     f'Socket {self.socket.getsockname()} closed with {length - len(data)} bytes left'
