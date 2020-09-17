d7018af83691df5e2e0eebb14911d01aa89a9c02
paveleroshkinweb <eroshkin321@gmail.com>
2020-09-17 17:36:29 +0300

    added string repr to entity

--- task1/src/models/entity.py
+++ task1/src/models/entity.py
@@ -5,3 +5,9 @@ class Entity:
 
     def to_json(self):
         return json.dumps(self.__dict__)
+
+    def __str__(self):
+        return self.to_json()
+
+    def __repr__(self):
+        return self.to_json()
