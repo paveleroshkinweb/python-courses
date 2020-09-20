b27b58f6cdbea8a2fce52a766c37df4cb19c3fc0
paveleroshkinweb <eroshkin321@gmail.com>
2020-09-20 21:36:20 +0300

    finished client side

--- task1/src/helpers/client_helper.py
+++ task1/src/helpers/client_helper.py
@@ -1,6 +1,10 @@
 import socket
-from sockets.app_socket import AppSocket
 from models.user import User
+from models.message import Message
+from models.message_types import MessageTypes
+from models.system_types import SystemTypes
+from models.commands import Commands
+import re
 
 
 class ClientHelper:
@@ -20,29 +24,97 @@ class ClientHelper:
         try:
             while not self.client_socket.closed:
                 self.recv_and_show_new_message()
-        except socket.error:
-            self.client_socket.exit(self.client_socket.format(AppSocket.UNKNOWN_PROBLEM), 1)
+        except socket.error as e:
+            self.client_socket.exit(
+                self.client_socket.format(self.client_socket.UNKNOWN_PROBLEM, e), 1
+            )
 
     def set_user(self):
         try:
-            text = input('Username: ')
-            message = self.parse_text(text)
+            name = input('Username: ').strip()
+            message = self._new_user_message(name)
             self.client_socket.send_message(message)
             response = self.recv_and_show_new_message()
             if not response.success:
                 self.set_user()
             else:
-                self.user = User(message.content)
+                self.user = User(message.name)
         except socket.error as e:
-            self.client_socket.exit(self.client_socket.format(AppSocket.UNKNOWN_PROBLEM, e), 1)
+            self.client_socket.exit(
+                self.client_socket.format(self.client_socket.UNKNOWN_PROBLEM, e), 1
+            )
 
     def listen_client_messages(self):
+        self.user = User('pety')
         try:
-            while not self.client_socket.closed:
-                text = input(f'{self.user} > ')
-                # TO DO
+            while True:
+            # while not self.client_socket.closed:
+                text = input(f'{self.user.name} > ').strip()
+                try:
+                    message = self.parse_text(text)
+                    print(message)
+                    # self.client_socket.send_message(message)
+                except ValueError as e:
+                    print(e)
         except socket.error as e:
-            self.client_socket.exit(self.client_socket.format(AppSocket.UNKNOWN_PROBLEM, e), 1)
+            self.client_socket.exit(
+                self.client_socket.format(self.client_socket.UNKNOWN_PROBLEM, e), 1
+            )
 
-    def parse_text(text):
-        pass
+    def _new_user_message(self, name):
+        return Message(**{
+            'message_type': MessageTypes.SYSTEM,
+            'content': name,
+            'system_type': SystemTypes.CREATE_NEW_USER,
+        })
+
+    def _exit_message(self):
+        return Message(**{
+            'message_type': MessageTypes.SYSTEM,
+            'from_whom': self.user.name,
+            'system_type': SystemTypes.EXIT
+        })
+
+    def _command_message(self, command, **kwargs):
+        return Message(**{
+            'message_type': MessageTypes.COMMAND,
+            'from_whom': self.user.name,
+            'command': command,
+            **kwargs
+        })
+
+    def _text_message(self, content):
+        return Message(**{
+            'message_type': MessageTypes.TEXT,
+            'from_whom': self.user.name,
+            'content': content
+        })
+
+    def _process_game_step(self, regex, text):
+        content = re.match(regex, text).group(1).strip()
+        return self._command_message(Commands.GAME_STEP, content=content)
+
+    def _process_private_message(self, regex, text):
+        res = re.match(regex, text)
+        selected_users = [user for user in res.group(1).strip().split(' ') if user != '']
+        content = res.group(2).strip()
+        if not selected_users or not content:
+            raise ValueError("Users to send and message can't be empty!")
+        return self._command_message(Commands.PRIVATE_MESSAGE,
+                                     selected_users=selected_users,
+                                     content=content)
+
+    def parse_text(self, text):
+        processors = [
+            (r'!EXIT!', lambda _: self._exit_message()),
+            (r'cmd!%s' % Commands.PARTICIPANTS_COUNT.value, lambda _: self._command_message(Commands.PARTICIPANTS_COUNT)),
+            (r'cmd!%s' % Commands.PARTICIPANTS.value, lambda _: self._command_message(Commands.PARTICIPANTS)),
+            (r'cmd!%s' % Commands.ROCK_PAPER_SCISSORS.value, lambda _: self._command_message(Commands.ROCK_PAPER_SCISSORS)),
+            (r'cmd!%s:(.+)' % Commands.GAME_STEP.value, lambda regex: self._process_game_step(regex, text)),
+            (r'cmd!%s(.+):(.+)' % Commands.PRIVATE_MESSAGE.value, lambda regex: self._process_private_message(regex, text)),
+            (r'.+', lambda _: self._text_message(text))
+        ]
+        for pattern, command in processors:
+            if re.match(pattern, text):
+                return command(pattern)
+        raise ValueError('Empty messages are not allowed!')
--- task1/src/models/commands.py
+++ task1/src/models/commands.py
@@ -1,10 +1,10 @@
 import enum
 
 
-class Command(str, enum.Enum):
-    ROCK_PAPER_SCISSORS = 'rock_paper_scissors',
-    PARTICIPANTS_COUNT = 'participants_count',
+class Commands(str, enum.Enum):
+    ROCK_PAPER_SCISSORS = 'rock-paper-scissors',
+    PARTICIPANTS_COUNT = 'participants-count',
     PARTICIPANTS = 'participants',
-    PRIVATE_MESSAGE = 'private_message',
-    GAME_STEP = 'game_step'
+    PRIVATE_MESSAGE = 'private-message',
+    GAME_STEP = 'game-step'
 
