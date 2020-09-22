84308ff2985b2cf5fdcd14b430e075b2480dfaeb
paveleroshkinweb <eroshkin321@gmail.com>
2020-09-22 15:02:06 +0300

    added ctrl+c handler

--- task1/src/client.py
+++ task1/src/client.py
@@ -1,10 +1,12 @@
 from sockets.client_socket import ClientSocket
 from utils.parse_cli_params import get_client_config
 import logging
+import signal
 
 
 if __name__ == '__main__':
     logging.basicConfig(level='INFO', format='%(asctime)s - %(message)s')
     client_config = get_client_config()
     client_socket = ClientSocket(client_config)
+    signal.signal(signal.SIGINT, lambda sig, frame: client_socket.exit('Clothing client ...', 1))
     client_socket.start()
--- task1/src/server.py
+++ task1/src/server.py
@@ -1,10 +1,12 @@
 import logging
 from sockets.server_socket import ServerSocket
 from utils.parse_cli_params import get_server_config
+import signal
 
 
 if __name__ == '__main__':
     logging.basicConfig(level='INFO', format='%(asctime)s - %(levelname)s - %(message)s')
     server_config = get_server_config()
     server_socket = ServerSocket(server_config)
+    signal.signal(signal.SIGINT, lambda sig, frame: server_socket.exit('Clothing server ...', 1))
     server_socket.start()
